a194456cfa98bdd226418f2ae4a784b6aa21369f
Olesia Romanienkova <110686374+Romanienkova@users.noreply.github.com>
2023-01-10 12:42:29 +0200

    1st

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  bgBody: document.body,
+};
+
+let timerId = null;
+	 
+refs.start.addEventListener('click', () => {
+  timerId = setInterval(() => {
+	  refs.bgBody.style.backgroundColor = getRandomHexColor();
+	  refs.start.disabled = true;
+	  refs.stop.disabled = false;
+  }, 1000);
+});
+
+refs.stop.addEventListener('click', () => {
+	clearInterval(timerId);
+	refs.stop.disabled = true;
+	refs.start.disabled = false;
+});
